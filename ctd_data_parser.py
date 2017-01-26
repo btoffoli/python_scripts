6215f78c47d6a7d34433e49837f92ee74c9221ae
Bruno R. Toffoli <bruno.toffoli@geocontrol.com.br>
2017-01-26 18:50:51 -0200

    finish ctd parser

--- ctd_data_parser.py
+++ ctd_data_parser.py
@@ -11,8 +11,9 @@ output_date_mask_sample_ref 	= "%Y%m%d-%H-%M"
 output_date_mask_csv_name	 	= "%Y%m%d_%H_%M"
 prefix 							= "CTD"
 separator 						= ","
-inputpath						= "/tmp"
-outputpath						= "/tmp"
+inputpath						= "/tmp/"
+outputpath						= "/tmp/"
+txt_separator = "\t"
 
 
 
@@ -48,8 +49,9 @@ def load_properties(file_path):
 def extract_p_and_date(line):
 	line_array					= line.split(separator)
 	depth   					= float(line_array[0])
-	p = "P1" if depth else "P3" if depth > 32 else "P2"
+	p = "P1" if depth < 10 else "P3" if depth > 32 else "P2"
 	reading_date				= datetime.strptime(line_array[11], input_date_mask)
+
 	return (p, reading_date)
 
 def extract_csv_info(line, line_number, p):
@@ -78,6 +80,56 @@ def extract_csv_info(line, line_number, p):
 
 	return resp_map
 
+def extract_txt_info(line):
+	line_array = line.split(separator)
+
+	depth 						= float(line_array[0])
+	sal 				 		= float(line_array[1])
+	temp 				 		= float(line_array[2])
+	cond 					 	= float(line_array[3])
+	dens 				 		= float(line_array[4])
+	sigma						= float(line_array[5])
+	chl 						= float(line_array[6])
+	turb						= float(line_array[7])
+	do_per						= float(line_array[8])
+	do_mg_l						= float(line_array[9])
+	nao_sei						= float(line_array[10])
+	reading_date				= datetime.strptime(line_array[11], input_date_mask)
+
+	day 						= reading_date.day
+	month 						= reading_date.month
+	year 						= reading_date.year
+	hour 						= reading_date.hour
+	minute 						= reading_date.minute
+	second 						= reading_date.second
+
+
+	resp_array = [
+		depth,
+		sal,
+		temp,
+		cond,
+		dens,
+		sigma,
+		chl,
+		turb,
+		do_per,
+		do_mg_l,
+		nao_sei,
+		day,
+		month,
+		year,
+		hour,
+		minute,
+		second
+	]
+
+	# resp_str = txt_separator.join(map(lambda x: str(x), resp_array))
+	resp_str = txt_separator.join(map(str, resp_array))
+	print(resp_str)
+
+	return resp_str
+
 
 
 
@@ -88,30 +140,62 @@ def parse():
 		"Sample Point", "Datasource", "Date", "Sample Ref"]
 
 	csv_files = []
-	for asc_filename in glob("%s/*.asc" % inputpath):
+	txt_files = []
+	p_map_count = {
+		'P1': 0,
+		'P2': 0,
+		'P3': 0
+	}
+	for asc_filename in glob("%s*.asc" % inputpath):
 		with open(asc_filename, mode="r") as asc_file:
-			p, first_date = extract_p_and_date(asc_file.readline())
-			asc_file.seek(0)
-			i = 1
-			csv_name = "%s/%s_%s_%s.csv" % (outputpath, prefix, p, first_date.strftime(output_date_mask_csv_name))
-			csvFile = open(csv_name, mode="w")
-			csvwriter = csv.DictWriter(csvFile,  fieldnames=array_header)
-			# writer.writerow(array_header)
-			csvwriter.writeheader()
-			for line in asc_file.readlines():
-				map_info = extract_csv_info(line, i, p)
-				csvwriter.writerow(map_info)
-				i += 1
+			try:
+				# print(asc_file.name)
+				p, first_date = (None,None)
+				for line in asc_file.readlines():
+					p, first_date = extract_p_and_date(line)
+				asc_file.seek(0)
+				i = 1
+				#CSV
+				csv_name = "%s%s_%s_%s.csv" % (outputpath, prefix, p, first_date.strftime(output_date_mask_csv_name))
+				csvFile = open(csv_name, mode="w")
+				csvwriter = csv.DictWriter(csvFile,  fieldnames=array_header)
+				csvwriter.writeheader()
 
-			csvFile.close()
-			csv_files.append(csvFile)
+				#TXT
+				p_map_count[p] += 1
+				txt_name = "%s%s_Relatorio_%s_%d.txt" % (outputpath, prefix, p, p_map_count[p])
+				txt_file = open(txt_name, mode="w")
 
-	return csv_files
+
+				for line in asc_file.readlines():
+					# CSV
+					map_info = extract_csv_info(line, i, p)
+					csvwriter.writerow(map_info)
+
+					# TXT
+					txt_line = extract_txt_info(line)
+					txt_file.write("%s\n" % txt_line)
+
+
+					i += 1
+
+				txt_file.close()
+				txt_files.append(txt_file)
+				csvFile.close()
+				csv_files.append(csvFile)
+			except Exception as e:
+				print("Erro %s em %s", str(e), asc_filename)
+			finally:
+				asc_file.close()
+
+
+	return (csv_files, txt_files)
 
 
 
 
 if __name__ == '__main__':
     caminho_arquivo_prop = argv[1] if len(argv) > 1 else 'ctd.conf'
-    csv_files = parse()
-    print("Arquivo gerado %s" % [file.name for file in csv_files])
+    files = parse()
+    print("Arquivos csv gerados %s" % [file.name for file in files[0]])
+    print("Arquivos txt gerados %s" % [file.name for file in files[1]])
